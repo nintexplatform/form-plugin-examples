c4c81ed409097e0714b9cd3025fd00af513c4bff
Aymen Al-Ashwal <aymen.alashwal@nintex.com>
2022-12-16 14:23:06 +1100

    feat: adding iframe example

---
+++ src/iframe.js
@@ -0,0 +1,76 @@
+import {css, html, LitElement, styleMap} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+
+export class ZwcIframe extends LitElement {
+    // Define scoped styles right with your component, in plain CSS
+    static styles = css`
+      :host {
+        height: 100%;
+        width: 100%;
+        display: block;
+      }
+
+      .frame {
+        display: inline-block;
+        height: 100%;
+        width: 100%;
+        background-color: transparent;
+        border: none;
+      }
+    `;
+
+    static getMetaConfig() {
+        // plugin contract information
+        return {
+            title: 'IFrame-new',
+            fallbackDisableSubmit: false,
+            description: 'IFrame component which can render url view with the frame',
+            iconUrl: 'https://designsystem.nintextest.com/icons/rich-text.svg',
+            groupName: 'Visual',
+            pluginAuthor: 'John Citizen',
+            version: '1.3',
+            properties: {
+                src: {
+                    type: 'string',
+                    title: 'Source URL',
+                    description: 'URL of the iframe, please note many sites block been rendered in iframes'
+                },
+                height: {
+                    type: 'string',
+                    title: 'Height',
+                    description: 'Height of the component',
+                }
+            },
+            standardProperties: {
+                readOnly: true,
+                required: true,
+                description: true,
+            }
+        };
+    }
+
+    static properties = {
+        name: 'Hello',
+        title: 'Hello',
+        src: 'https://www.wikipedia.org/',
+        height: '100%'
+    }
+
+    // Render the UI as a function of component state
+    render() {
+        let styles = {height: this.height};
+
+        return html`
+            <iframe
+                    class="frame"
+                    style=${styleMap(styles)}
+                    name=${this.name}
+                    allow="geolocation *; microphone; camera"
+                    title=${this.title}
+                    src=${this.src}
+            ></iframe>`;
+    }
+}
+
+// registering the web component.
+const elementName = 'zwc-iframe-new';
+customElements.define(elementName, ZwcIframe);
