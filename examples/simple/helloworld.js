6665e922ba789cf9d704fc630e0041baea74cf96
SeiDei <118710549+SeiDei@users.noreply.github.com>
2023-01-06 12:33:21 +1100

    Create helloworld.js
    You can use any modern web component framework to build Form plugins. In this example, we will build a basic plugin in a single JavaScript file that displays some static HTML. We will build the standard web component first using Lit, and then add the Form plugin contract to convert it to a plugin.

---
+++ examples/simple/helloworld.js
@@ -0,0 +1,21 @@
+import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+​
+// define the component
+export class CustomInput extends LitElement {
+  // return a promise for contract changes.
+  static async getMetaConfig() {
+    return {
+      controlName: 'Hello World',
+      fallbackDisableSubmit: false,
+      version: '1.2',
+    };
+  }
+​
+  render() {
+    return html`<input placeholder="Hello World" />`;
+  }
+}
+​
+// registering the web component
+const elementName = 'hello-world';
+customElements.define(elementName, CustomInput);
