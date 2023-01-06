2b45b93da6afd2355cb9e124f05c3945aa4a0ac1
Carl <carl.hickton@gmail.com>
2023-01-06 13:04:27 +1100

    Update helloworld.js

--- examples/simple/helloworld.js
+++ examples/simple/helloworld.js
@@ -1,17 +1,34 @@
 import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 // define the component
 export class HelloWorld extends LitElement {
+  
+  static properties = {
+    who: {type: String},
+  };
+  
   // return a promise for contract changes.
-  static async getMetaConfig() {
+  static getMetaConfig() {
     return {
       controlName: 'Hello World',
       fallbackDisableSubmit: false,
       version: '1.2',
+      properties: {
+        src: {
+          type: 'string',
+          title: 'Who',
+          description: 'Who to say hello to'
+        }
+      }
     };
+  }
+  
+  constructor() {
+    super();
+    this.who = 'World';
   }
 
   render() {
-    return html`<p>Hello World<p/>`;
+    return html`<p>Hello ${this.who}<p/>`;
   }
 }
 
