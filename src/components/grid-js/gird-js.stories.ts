d46ad983ab2c67f5aa44ab0b82717ff3164178b9
Carl <carl.hickton@gmail.com>
2023-01-30 16:22:43 +1100

    feat: adding grid example (#7)

---
+++ src/components/grid-js/gird-js.stories.ts
@@ -0,0 +1,21 @@
+import { Meta, Story } from '@storybook/web-components';
+import { html } from 'lit';
+
+import './grid-js';
+import { NintexSampleGirdJs } from './grid-js';
+
+export default {
+  title: 'Grid JS',
+  component: 'nintex-sample-grid-js',
+} as Meta;
+
+const Template: Story<NintexSampleGirdJs> = ({ sortable, pagination }) => {
+  return html`<nintex-sample-grid-js
+    ?sortable=${sortable}
+    ?pagination=${pagination}
+  >
+  </nintex-sample-grid-js>`;
+};
+
+export const Base: Story<NintexSampleGirdJs> = Template.bind({});
+Base.args = {};
