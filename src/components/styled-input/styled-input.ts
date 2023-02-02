ab9ee27f85bc491379fdf3ac81dddda60b71513b
Aymen Al-Ashwal <aymen.alashwal@nintex.com>
2023-02-02 11:12:21 +1100

    feat: add-styled-plugin-example

---
+++ src/components/styled-input/styled-input.ts
@@ -0,0 +1,77 @@
+import { css, html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import { NintexPlugin } from '../../lib/nintex-plugin';
+
+@customElement('ntx-styled-input')
+export class StyledInput extends LitElement {
+  static styles = css`
+    .form-control {
+      color: var(--ntx-form-theme-color-secondary);
+      background-color: var(
+        --ntx-form-theme-color-input-background,
+        transparent
+      );
+      font-size: var(--ntx-form-theme-text-input-size);
+      font-family: var(--ntx-form-theme-font-family);
+      border: 1px solid var(--ntx-form-theme-color-border);
+      border-radius: var(--ntx-form-theme-border-radius);
+    }
+
+    .form-control:focus {
+      outline: none;
+      border-color: var(--ntx-form-theme-color-primary);
+    }
+  `;
+
+  @property({ type: Boolean })
+  readOnly: boolean = false;
+
+  @property({ type: String })
+  value: boolean = false;
+
+  static getMetaConfig(): Promise<NintexPlugin> | NintexPlugin {
+    // plugin contract information
+    return {
+      controlName: 'Styled Input',
+      fallbackDisableSubmit: false,
+      iconUrl: 'one-line-text',
+      version: '1',
+      properties: {
+        value: {
+          type: 'string',
+          title: 'Value',
+          // this is to mark the field as value field. it should only be defined once in the list of properties
+          isValueField: true,
+          defaultValue: 'This is a text field default value',
+        },
+      },
+      standardProperties: {
+        fieldLabel: true,
+        defaultValue: true,
+        readOnly: true,
+      },
+    };
+  }
+
+  // Render the UI as a function of component state
+  render() {
+    return html`<input
+      class='form-control'
+      ?disabled='${this.readOnly}'
+      @change='${(e: any) => this.onChange(e)}'
+      value='${this.value}'
+    ></input>`;
+  }
+
+  private onChange(e: any) {
+    const value = e.target?.value;
+    const args = {
+      bubbles: true,
+      cancelable: false,
+      composed: true,
+      detail: value,
+    };
+    const event = new CustomEvent('ntx-value-change', args);
+    this.dispatchEvent(event);
+  }
+}
