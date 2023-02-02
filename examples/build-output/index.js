65f76019749ba5f3a93fd53c13315998788bba06
Aymen Al-Ashwal <aymen.alashwal@nintex.com>
2023-02-02 13:17:38 +1100

    chore: add build output example

--- examples/build-output/index.js
+++ examples/build-output/index.js
@@ -1,4 +1,5 @@
 export { NintexSampleIframe } from './iframe.js';
 export { NintexSampleTextfield } from './material-textfield.js';
+export { NintexStyledInput } from './styled-input.js';
 import './query-assigned-elements-5558b813.js';
 import './directive-2bb7789e.js';
